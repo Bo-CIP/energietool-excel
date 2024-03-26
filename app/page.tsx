7eac41fdd453e987a6430c2bbdd07885c6d4d38c
Bo-CIP <bo@corporateip.nl>
2024-03-26 10:34:11 +0100

    Design is onderweg

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,153 @@
-import Image from "next/image";
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
-export default function Home() {
+export default function CardWithForm() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+    {/*Eerste rij aan invoer velden op cards*/}
+
+    {/*Eerste card */}
+
+    <div className="flex flex-row">
+      <div className="flex flex-col">
+      <Card className="w-[560px] h-[280px] ml-36 mt-24">
+        <CardHeader>
+          <CardTitle className="mb-2">Huidig energieverbruik</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label className="mb-1" htmlFor="name">Elektriciteit van energiemij (kWh eigen zonnepanelen niet aftrekken)</Label>
+                <Input id="elektriciteit" placeholder="Uw waarden" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label className="mb-1" htmlFor="framework">Gas m3 (maximaal 20.000 m3)</Label>
+                <Input id="gas-m3" placeholder="Uw waarden"></Input>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+        </CardFooter>
+      </Card>
+
+      {/* Einde eerste card */}
+
+      {/* Begin tweede card */}
+
+      <Card className="w-[560px] mt-12 ml-36 ">
+        <CardHeader>
+          <CardTitle className=""></CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label className="mb-1" htmlFor="name">Aantal gezinsleden? Vul 0 in bij bedrijf zonder tapwater op gas</Label>
+                <Input id="elektriciteit" placeholder="Uw waarden" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label className="mb-1" htmlFor="framework">Lager gasverbruik door hybride verwarming of airco (60-100%) klik op 90%</Label>
+                <Select>
+                  <SelectTrigger id="framework">
+                    <SelectValue placeholder="Kies uit..." />
+                  </SelectTrigger>
+                  <SelectContent position="popper">
+                    <SelectItem value="60">60%</SelectItem>
+                    <SelectItem value="70">70%</SelectItem>
+                    <SelectItem value="80">80%</SelectItem>
+                    <SelectItem value="90">90%</SelectItem>
+                    <SelectItem value="100">100%</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label className="" htmlFor="framework">Oppervlak woning/bedrijf in m2 t.b.v. berekening energielabel</Label>
+                <Input id="elektriciteit" placeholder="Uw waarden" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label className="mb-1" htmlFor="framework">Correctie 50-100% extra gasverbruik voor onregelmatig gebruik klik op 0%</Label>
+                <Select>
+                  <SelectTrigger id="framework">
+                    <SelectValue placeholder="Kies uit..." />
+                  </SelectTrigger>
+                  <SelectContent position="popper">
+                    <SelectItem value="50">50%</SelectItem>
+                    <SelectItem value="60">60%</SelectItem>
+                    <SelectItem value="70">70%</SelectItem>
+                    <SelectItem value="80">80%</SelectItem>
+                    <SelectItem value="90">90%</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+        </CardFooter>
+      </Card>
+
+      {/*Einde tweede card */ }
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/*Einde eerste rij aan invoer velden op cards */}
+
+
+      
+      {/* Begin tweede rij aan invoer velden op cards*/}
+
+
+    <Card className="w-[560px] h-[400px] mt-5 ml-24 mt-24 mb-1">
+        <CardHeader>
+          <CardTitle className=""></CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label className="mb-1" htmlFor="name">Aantal gezinsleden? Vul 0 in bij bedrijf zonder tapwater op gas</Label>
+                <Input id="elektriciteit" placeholder="Uw waarden" />
+              </div>
+              
+              <div className="flex flex-col space-y-1.5">
+                <Label className="" htmlFor="framework">Oppervlak woning/bedrijf in m2 t.b.v. berekening energielabel</Label>
+                <Input id="elektriciteit" placeholder="Uw waarden" />
+              </div>
+              
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+        </CardFooter>
+      </Card>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  
+      {/*Einde tweede rij aan invoer velden op cards */}
+    
+      
+  </>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
 }
