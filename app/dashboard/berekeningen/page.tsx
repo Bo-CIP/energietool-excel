4628334ece782e2fac287164e45e7c8b97e24d1d
Bo-CIP <bo@corporateip.nl>
2024-04-05 18:42:55 +0200

    Framing van het project is gedaan

---
+++ app/dashboard/berekeningen/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BerekenenDashboard from '@/components/berekeningen'
+
+export default function Berekeningen() {
+
+    return(
+        <>
+            <BerekenenDashboard>
+                
+            </BerekenenDashboard>
+        </>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { LandingsDashboard } from "@/components/dashboard";
+
+
+export default function LandingPage()
+{
+    return(
+        <>
+        <LandingsDashboard></LandingsDashboard>
+        </>
+    )
+}
